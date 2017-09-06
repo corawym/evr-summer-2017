14cd348ae291103f22d60e666467503da8b25b14
corawym <corawym@gmail.com>
2017-09-05 23:42:14 -0700

    added main site menu

--- themes/evr/header.php
+++ themes/evr/header.php
@@ -23,10 +23,15 @@
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+					<p class="site-description screen-reader-text"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<div>
+						<a href="#">
+							<img src="" alt="East Van Roasters's logo">
+						</a>
+					</div>
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav><!-- #site-navigation -->
