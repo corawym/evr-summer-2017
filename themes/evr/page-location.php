c4f78982a97bc32397e3050a90b2c8d5db25e101
corawym <corawym@gmail.com>
2017-09-07 15:59:20 -0700

    fixed location page hero banner on mobile and desktop

--- themes/evr/page-location.php
+++ themes/evr/page-location.php
@@ -9,12 +9,14 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-      <section class="location-hero-container container-full">
-        <div class="location-hero-content container-padding">
-          <h1 class="hero-content-title">location</h1>
-          <p class="hero-content-description">East Van Roasters (319 Carrall Street in the Rainier Hotel) is 
-            a 16-seat specialty cafe and social enterprise of the PHS Community Services Society.</p> 
-          <a class="hero-button" href="#">Learn More <i class="fa fa-fw fa-arrow-circle-down" aria-hidden="true"></i></a>
+      <section class="hero-banner container-full">
+        <div class="hero-content-container container-padding">
+          <div class="hero-content container-max1080">
+            <h1 class="hero-content-title">location</h1>
+            <p class="hero-content-description">East Van Roasters (319 Carrall Street in the Rainier Hotel) is 
+              a 16-seat specialty cafe and social enterprise of the PHS Community Services Society.</p> 
+            <a class="hero-button" href="#">Learn More <i class="fa fa-fw fa-arrow-circle-down" aria-hidden="true"></i></a>
+          </div>
         </div>
       </section>
 
