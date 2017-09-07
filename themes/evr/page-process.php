980d1dc37e36d3f8c98698e6161e2ff0982d22bf
jefe <jefe@fleet.local>
2017-09-06 20:37:51 -0700

    added variables to sass

--- themes/evr/page-process.php
+++ themes/evr/page-process.php
@@ -8,13 +8,10 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main process" role="main">
+			<img class="process-hero" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/progress-1.png" alt="Roasting Coffee">
 
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
+			
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
