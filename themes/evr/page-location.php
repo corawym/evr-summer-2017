f6a283b8f5192ec0974a19127932b931c8c05186
corawym <corawym@gmail.com>
2017-09-07 04:04:16 -0700

    added location page content

---
+++ themes/evr/page-location.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * The template for display location page.
+ *
+ * @package EVR_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<section class="location-hero">
+        <h1></h1>
+      </section>
+
+      <section class="location-evr-info">
+        <h2>Location</h2>
+        <div class="evr-info-wrapper">
+          <p>319 Caraall Street, Vancouver, BC V6B 2J4</p>
+        </div>
+        <div class="evr-info-wrapper">
+          <h3>Phone</h3>
+          <p>(604) 629 7562</p>
+        </div>
+        <div class="evr-info-wrapper">
+          <h3>Hours</h3>
+          <div class="location-date">
+            <p>Tuesday</p>             
+            <p>Wednesday</p>        
+            <p>Thursday</p>            
+            <p>Friday</p>                 
+            <p>Saturday</p>         
+            <p>Sunday</p>               
+            <p>Monday</p>    
+          </div>
+          <div class="location-hours">
+            <p>9 AM - 5 PM</p> 
+            <p>9 AM - 5 PM</p>
+            <p>9 AM - 5 PM</p>
+            <p>9 AM - 5 PM</p>
+            <p>10 AM - 5 PM</p>
+            <p>Closed</p>
+            <p>Closed</p>
+          </div>
+        </div>
+        <div class="evr-info-wrapper">
+          <h3>Email</h3>
+          <p>info@eastvanroasters.com</p>
+        </div>
+      </section>
+
+      <section class="location-evr-partners">
+        <h2>Our chocolate and coffee beans can also be found at a select few locations in Vancouver</h2>
+        
+        <div class="location-evr-partners-chocolate-coffee">
+          <h3>Chocolate Bars & Coffee Beans</h3>
+          <div>
+            <p>Edible Canada</p>
+            <p>1596 Johnston Street</p>
+            <p>Vancouver, BC</p>
+            <p>T 604  683 6681</p>
+            <p>https://ediblecanada.com/</p>
+          </div>
+          <div>
+            <p>SPUD Urban Delivery</p>
+            <p>www.spud.ca</p>
+          </div>
+          <div>
+            <p>Bars online</p>
+            <p>La Tablette de Miss Choco</p>
+            <p>http://www.misschoco.ca/en/</p>
+          </div>
+        </div>
+
+        <div class="location-evr-partners-chocolate">
+          <h3>Seasonal Chocolates</h3>
+          <div>
+            <p>Harvest Community Foods</p>
+            <p>243 Union Street</p>
+            <p>Vancouver  BC</p>
+            <p>T 604  682 8851</p>
+            <p>http://harvestunion.ca</p>
+          </div>
+          <div>
+            <p>Le Marche St. George</p>
+            <p>4393 St. George at E.28th Ave</p>
+            <p>Vancouver  BC</p>
+            <p>T 604  565 5107</p>
+            <p>http://www.marchestgeorge.com</p>
+          </div>
+          <div>
+            <p>Nineteen Ten Home Boutique</p>
+            <p>4366 Main Street</p>
+            <p>Vancouver, BC</p>
+            <p>T 604 558 0210</p>
+            <p>http://www.nineteenten.ca</p>
+          </div>
+        </div>
+
+        <div class="location-evr-partners-coffee">
+          <h3>Coffee on the Bar</h3>
+          <div>
+            <p>Lupii cafe</p>
+            <p>7743 Champlain Cres</p>
+            <p>Vancouver, BC</p>
+            <p>T 778 379  7959</p>
+            <p>www.lupiicafe.com</p>
+          </div>
+          <div>
+            <p>Nourish Café</p>
+            <p>Nourish Café & Cooking School</p>
+            <p>3742 W. 10th Ave</p>
+            <p>Vancouver, BC</p>
+            <p>T. 604-222-8350</p>
+            <p>https://www.nourishvancouver.ca/pages/cooking-school-cooking-classes-vancouver</p>
+          </div>
+        </div>
+
+      </section>
+
+      
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
