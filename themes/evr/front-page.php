0272762ce74adc998d33c1e62d61e1ee7eb59a0c
ShaahidKhalpey <shaahidkhalpey@gmail.com>
2017-09-08 13:25:44 -0700

    added quotations

--- themes/evr/front-page.php
+++ themes/evr/front-page.php
@@ -59,7 +59,7 @@ get_header(); ?>
 
         <section class="content-two">
             <div class="content-two-div">
-                <span class="quote1"></span><p>Trust me, if you stop by and have a cup of the intense Mayan Drinking Chocolate, you'll feel the clouds part and the angels sing.</p><span class="quote2"></span>
+                <span class="open-quote"></span><p>Trust me, if you stop by and have a cup of the intense Mayan Drinking Chocolate, you'll feel the clouds part and the angels sing.</p><span class="quote2"></span>
                 <p class="author">John Crawford,<span class="eat"> Eat Magazine</span></p>
             </div>
         </section>
