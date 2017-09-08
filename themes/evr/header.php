747b5efeb50bd15d75df2aa5be8d1006955b227e
temefleet <jefftemes@gmail.com>
2017-09-07 18:38:00 -0700

    Submenu toggling properly

--- themes/evr/header.php
+++ themes/evr/header.php
@@ -40,3 +40,4 @@
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
+			
--- themes/evr/js/submenu-toggle.js
+++ themes/evr/js/submenu-toggle.js
@@ -1,17 +1,18 @@
 (function($) {
-  $('#menu-item-37 > .sub-menu').hide();
-  
-  $('#menu-item-37').hover(function() {
-    event.preventDefault();
-  })
+  if ($(window).width() < 800) {
+    $('#menu-item-37 > .sub-menu').addClass('hide-sub-menu');
+    
+    $('#menu-item-37').hover(function() {
+      event.preventDefault();
+    })
 
-  $('#menu-item-37').click(function() {
-    var submenu = $(this).children('.sub-menu');
-    if ( $(submenu).is(':hidden') ) {
-      $(submenu).slideDown(200);
-    } else {
-      $(submenu).slideUp(200);
-    }
-  });
+    $('#menu-item-37').click(function() {
+      var subMenu = $(this).children('.sub-menu');
+      if ( $(subMenu).hasClass('hide-sub-menu') ) {
+        $(subMenu).removeClass('hide-sub-menu');
+      } else {
+        $(subMenu).addClass('hide-sub-menu');
+      }
+    });
+  }
 })(jQuery);
-
