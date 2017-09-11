e008f087f35b9b48b5e81bc46732a2afc660e728
corawym <corawym@gmail.com>
2017-09-11 15:59:41 -0700

    modified donate page content style

--- themes/evr/page-donate.php
+++ themes/evr/page-donate.php
@@ -21,10 +21,28 @@ get_header(); ?>
       </section>
 
       <section id="donate-content">
-        <h2>We are a fully registered charity and would love to have your support!</h2>
-        <p>You can choose the proceeds to go to</p>
-        <p>Suport the  women in the program. The proceeds will be given as honorarium to our peers.</p>
-        <p>Help us raise funds to hire more staff and purchase equiments needed to increase our production capacity.</p>
+        <div class="container-white-bg-content container-separator-top-bottom">
+          <h3>East Van Roasters is a non-profit initiative benefiting the women residents of the Rainier Hotel.</h3>
+          <p>We are a fully registered charity and would love to have your support!</p>
+          <div class="container-separator-top-bottom">
+            <h3 class="text-center">You can choose the proceeds to go to:</h3>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/donate/donation-icon-1.svg" alt="A icon of donation box">
+            <p class="text-center">Suport the  women in the program. The proceeds will be given as honorarium to our peers.</p>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/donate/donation-icon-2.svg" alt="A icon of cooking tools">
+            <p class="text-center">Help us raise funds to hire more staff and purchase equiments needed to increase our production capacity.</p>
+          </div>
+          <div>
+            <h3 class="text-center">Three ways you can support:</h3>
+            <ol>
+              <li>Send a cheque (payable to East Van Roasters) to 319 Carrall Street, Vancouver, BC, V6B 2J4. 
+                If your donation exceeds $50.00 we are happy to provide a charitable tax receipt - 
+                we ask that you include your name and return address, as well as a self-addressed stamped envelope if possible.</li>
+              <li>Stop by the shop and donate by cash, debit or credit card.</li>
+              <li>Purchase our products.</li>
+            </ol>
+
+          </div>
+        </div>
       </section>
 
 
