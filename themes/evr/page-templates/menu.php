1e3e1a30e33528d5657c08045ca8b0fb11a3f638
corawym <corawym@gmail.com>
2017-09-12 16:56:48 -0700

    got menu datas and added menu styles

--- themes/evr/page-templates/menu.php
+++ themes/evr/page-templates/menu.php
@@ -22,38 +22,66 @@ get_header(); ?>
 			<section class="menu-content-container container-full" id="menu-content">
 				<div class="menu-content container-full">
 
-					<section class="menu-coffee-container container-full">
-						<div class="menu-coffee-content">
+					<section class="menu-coffee-container container-full container-separator-top-bottom">
+						<div class="menu-coffee-content container-max800 text-center">
 							<h2>From the Coffee Bar</h2>
 							<div class="menu-line"></div>
 							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/menu/chocolate-bar.svg" alt="A Coffer Bar icon">
 							<p>Our single-origin drinking chocolates and espresso drinks are made in house with our freshly roasted beans. All espresso bar drinks are made with organic dairy, almond or soy milk.</p>
 							
 							<!-- Below is coffee bar menu -->
-							<div class="menu-evr-menu-container">
-								<?php
-									$args = array( 
-										'post_type' => 'menu', 
-										'posts_per_page' => 4, 
-										'orderby' => 'date',
-										'order' => 'ASC'
-										);
-									$menu_items = get_posts( $args ); // returns an array of posts
-								?>
-								
-								<?php foreach ( $menu_items as $post ) : setup_postdata( $post ); ?>
-									<h3 class="menu-section-title"><?php echo CFS()->get('coffees'); ?></h3>
-									<p><?php echo CFS()->get('coffee_name'); ?></p>
-								<?php endforeach; wp_reset_postdata(); ?>
+							<div class="menu-evr-menu-container display-flex-wrap">
+
+								<!--Below is menu coffees section-->
+								<div class="evr-menu-section evr-menu-coffee display-flex-wrap">
+									<h3 class="menu-section-title">Coffees</h3>
+									<?php	$coffees = CFS()->get( 'coffees' );
+										foreach ( $coffees as $coffee_items ) { ?>
+											<p class="menu-items-name"><?php echo $coffee_items['coffee_name']; ?></p>
+											<p class="menu-items-price"><?php echo $coffee_items['coffee_price']; ?></p>
+									<?php } ?>
+								</div>
+
+								<!--Below is menu iced drinks section-->
+								<div class="evr-menu-section evr-menu-iced-drink display-flex-wrap">
+									<h3 class="menu-section-title">Iced Drinks</h3>
+									<?php	$iced_drinks = CFS()->get( 'iced_drinks' );
+										foreach ( $iced_drinks as $iced_drink_items ) { ?>
+											<p class="menu-items-name"><?php echo $iced_drink_items['iced_drink_name']; ?></p>
+											<p class="menu-items-price"><?php echo $iced_drink_items['iced_drink_price']; ?></p>
+									<?php } ?>
+								</div>
+
+								<!--Below is menu full leaf tea section-->
+								<div class="evr-menu-section evr-menu-full-leaf-tea display-flex-wrap">
+									<h3 class="menu-section-title">Full Leaf Teas</h3>
+									<div class="menu-items-name-container">
+										<?php	$full_leaf_teas = CFS()->get( 'full_leaf_teas' );
+											foreach ( $full_leaf_teas as $full_leaf_tea_items ) { ?>
+												<p class="menu-items-name"><?php echo $full_leaf_tea_items['full_leaf_tea_name']; ?></p>
+										<?php } ?>
+									</div>
+									<p class="menu-items-price"><?php echo CFS()->get('full_leaf_tea_price'); ?></p>
+								</div>
+
+								<!--Below is menu drinking chocolates section-->
+								<div class="evr-menu-section evr-menu-drinking-chocolate display-flex-wrap">
+									<h3 class="menu-section-title">Drinking Chocolates</h3>
+									<div class="menu-items-name-container">
+									<?php $drinking_chocolates = CFS()->get( 'drinking_chocolates' );
+										foreach ( $drinking_chocolates as $drinking_chocolate_items ) { ?>
+											<p class="menu-items-name"><?php echo $drinking_chocolate_items['drinking_chocolate_name']; ?></p>
+									<?php } ?>
+									</div>
+									<p class="menu-items-price"><?php echo CFS()->get('drinking_chocolate_price'); ?></p>
+								</div>	
 							</div>
 
 						</div>
-						
-						
 					</section><!-- end of .menu-coffee-container -->
 
-					<section class="menu-chocolate-container">
-						<div class="menu-chocolate-content">
+					<section class="menu-chocolate-container container-max800 container-full container-separator-top-bottom">
+						<div class="menu-chocolate-content text-center">
 							<h2>From the Chocolate Lab</h2><div class="menu-line"></div>
 							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/menu/chocolate-bar.svg" alt="A Chocolate Bar icon">
 							<p>We source and use organic fair-trade and direct-trade cacao beans , processing and handcrafting them in-house to produce finished bars that boast regional flavours from the Dominican Republic, Madagascar and Peru. 
@@ -61,7 +89,7 @@ get_header(); ?>
 						</div>
 
 						<!-- Below is the chocolate lab items -->
-						<div class="menu-chocolate-grid">
+						<div class="menu-chocolate-grid display-flex-wrap">
 							<?php
 								$args = array( 
 									'post_type' => 'chocolate', 
@@ -88,7 +116,7 @@ get_header(); ?>
 				</div>
 			</section><!-- end of .menu-content-container -->
 
-			<section class="menu-location">
+			<section class="menu-location container-max800 text-center">
 				<p>Our chocolate and coffee beans can also be found at a select few <span><a href="#">locations</a></span> in Vancouver.</p>
 			</section>
 			
