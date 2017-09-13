08749d9db701364025778d21bbd88bbdbe02f57c
ShaahidKhalpey <shaahidkhalpey@gmail.com>
2017-09-13 13:31:24 -0700

    modified modal javascript file

--- themes/evr/js/modal-subscribe.js
+++ themes/evr/js/modal-subscribe.js
@@ -1,9 +1,9 @@
 (function ($) {
     $(document).ready(function () {
-        if (localStorage.getItem('popState') !== 'shown') {
-            $("#email-popup").delay(2000).fadeIn();
-            localStorage.setItem('popState', 'shown')
-        }
+        // if (localStorage.getItem('popState') !== 'shown') {
+            $('#email-popup').delay(2000).fadeIn();
+        //     localStorage.setItem('popState', 'shown')
+        // }
         $('#close-popup, #email-popup').click(function () {
                 $('#email-popup').fadeOut(); 
             });
