4c87376a8ed8c74cd5c34eaa146e60d9ed4ffc64
ShaahidKhalpey <shaahidkhalpey@gmail.com>
2017-09-13 10:44:48 -0700

    updated modal scripts

--- themes/evr/js/modal-subscribe.js
+++ themes/evr/js/modal-subscribe.js
@@ -1,40 +1,11 @@
-(function($) {
-
-    // var formHtml = '' +
-    // '<div class="email-popup">' +
-    //     '<div class="popup-header">' +
-    //         '<h1>Newsletter</h1>' +
-    //         '<div class="close-popup">' +
-    //         '</div>' +
-    //     '</div>' +
-    //     '<div class="popup-content">' +
-    //         '<h2>Get on the list today!</h2>' +
-    //         '<p>Sign up with your email address for exclusive events and products</p>' +
-    //         '<form class="subscribe-form" action="subscribe">' +
-    //             '<input class="email" type="email" name="usremail" placeholder="Enter Email">' +
-    //             '<button class="sign-up" type="submit">Sign Up</button>' +
-    //         '</form>' +
-    //     '</div>' +
-    // '</div>';
-
-    // setTimeout(function(){ 
-    //     $('#email-popup').css('display', 'block');
-    // }, 2000);
-
-    // $('#close-popup').click(function() {
-    //     console.log('heyheyhey');
-    //     $('#email-popup').fadeOut(300);
-    // });
-
-    $(document).ready(function() {
-        if(localStorage.getItem('popState') !== 'shown'){
+(function ($) {
+    $(document).ready(function () {
+        if (localStorage.getItem('popState') !== 'shown') {
             $("#email-popup").delay(2000).fadeIn();
-            localStorage.setItem('popState','shown')
+            localStorage.setItem('popState', 'shown')
         }
-    
-        $('#close-popup, #email-popup').click(function() // You are clicking the close button
-        {
-            $('#email-popup').fadeOut(); // Now the pop up is hiden.
-        });
+        $('#close-popup, #email-popup').click(function () {
+                $('#email-popup').fadeOut(); 
+            });
     });
 })(jQuery);
