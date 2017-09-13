bebc4bef4723f9acefa00e4d3288eb61c8d23499
temefleet <jefftemes@gmail.com>
2017-09-13 10:54:38 -0700

    Removed cookie until production deployment

--- themes/evr/js/modal-subscribe.js
+++ themes/evr/js/modal-subscribe.js
@@ -1,11 +1,11 @@
 (function ($) {
     $(document).ready(function () {
-        if (localStorage.getItem('popState') !== 'shown') {
-            $("#email-popup").delay(2000).fadeIn();
-            localStorage.setItem('popState', 'shown')
-        }
+        // if (localStorage.getItem('popState') !== 'shown') {
+            $('#email-popup').delay(2000).fadeIn();
+            // localStorage.setItem('popState', 'shown');
+        // }
         $('#close-popup, #email-popup').click(function () {
-                $('#email-popup').fadeOut(); 
+                $('#email-popup').fadeOut(100); 
             });
     });
 })(jQuery);
