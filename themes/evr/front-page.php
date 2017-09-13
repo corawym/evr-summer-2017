3d618c9cc71518c4f3ac8200079a21b2ee4c0f79
ShaahidKhalpey <shaahidkhalpey@gmail.com>
2017-09-12 23:54:43 -0700

    changed popup to show only on home page

--- themes/evr/front-page.php
+++ themes/evr/front-page.php
@@ -29,6 +29,22 @@ get_header(); ?>
                     </div>
                 </div>
             </div>
+
+            <div class="email-popup">
+                <div class="popup-header">
+                    <h1>Newsletter</h1>
+                    <div class="close-popup">
+                    </div>
+                </div>
+                <div class="popup-content">
+                    <h2>Get on the list today!</h2>
+                    <p>Sign up with your email address for exclusive events and products</p>
+                    <form class="subscribe-form" action="subscribe">
+                        <input class="email" type="email" name="usremail" placeholder="Enter Email">
+                        <button class="sign-up" type="submit">Sign Up</button>
+                    </form>
+                </div>
+            </div>;
         </section>
 
         <section class="content-one">
--- themes/evr/js/modal-subscribe.js
+++ themes/evr/js/modal-subscribe.js
@@ -1,28 +1,28 @@
 (function($) {
 
-    var formHtml = '' +
-    '<div class="email-popup">' +
-        '<div class="popup-header">' +
-            '<h1>Newsletter</h1>' +
-            '<div class="close-popup">' +
-            '</div>' +
-        '</div>' +
-        '<div class="popup-content">' +
-            '<h2>Get on the list today!</h2>' +
-            '<p>Sign up with your email address for exclusive events and products</p>' +
-            '<form class="subscribe-form" action="subscribe">' +
-                '<input class="email" type="email" name="usremail" placeholder="Enter Email">' +
-                '<button class="sign-up" type="submit">Sign Up</button>' +
-            '</form>' +
-        '</div>' +
-    '</div>';
+    // var formHtml = '' +
+    // '<div class="email-popup">' +
+    //     '<div class="popup-header">' +
+    //         '<h1>Newsletter</h1>' +
+    //         '<div class="close-popup">' +
+    //         '</div>' +
+    //     '</div>' +
+    //     '<div class="popup-content">' +
+    //         '<h2>Get on the list today!</h2>' +
+    //         '<p>Sign up with your email address for exclusive events and products</p>' +
+    //         '<form class="subscribe-form" action="subscribe">' +
+    //             '<input class="email" type="email" name="usremail" placeholder="Enter Email">' +
+    //             '<button class="sign-up" type="submit">Sign Up</button>' +
+    //         '</form>' +
+    //     '</div>' +
+    // '</div>';
 
     setTimeout(function(){ 
-        $('.home').append(formHtml);
+        $('.home').append('.email-popup');
     }, 2000);
 
-    $(formHtml).click(function() {
-        // $('html').remove(formHtml);
+    $('.close-popup').click(function() {
+        $('.email-popup').hide();
         console.log('hey')
     });
 })(jQuery);
