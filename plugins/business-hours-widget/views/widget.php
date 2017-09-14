5e42742a5c8690dace38cb076210068cd2b2857d
temefleet <jefftemes@gmail.com>
2017-09-14 16:38:54 -0700

    updated business hours widget

--- plugins/business-hours-widget/views/widget.php
+++ plugins/business-hours-widget/views/widget.php
@@ -1,4 +1,10 @@
 <!-- This file is used to markup the public-facing widget. -->
+<?php if ( strlen( trim( $days ) ) > 0) : ?>
+  <p>
+    <span class="day-of-week"></span><?php echo $days ?>
+  </p>
+<?php endif; ?>
+
 <?php if ( strlen( trim( $monday_friday ) ) > 0) : ?>
   <p>
     <span class="day-of-week">Monday-Friday: </span><?php echo $monday_friday ?>
