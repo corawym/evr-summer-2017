da347867925345dc891a3fc02fa2e42dd503d8e5
ShaahidKhalpey <shaahidkhalpey@gmail.com>
2017-09-15 16:16:51 -0700

    made edits to locations

--- themes/evr/page-templates/location.php
+++ themes/evr/page-templates/location.php
@@ -6,15 +6,11 @@
  */
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
 
-			
-
-
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
